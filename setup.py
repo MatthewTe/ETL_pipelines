60072fe66a3d9fa840b36f80014b4f8edd096f58
MatthewTee <teelucksingh.matthew1@gmail.com>
2021-01-09 15:24:47 -0400

    Added Additional Setup Config Commits

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name = "ETL Pipeline API",
     url = "https://github.com/MatthewTe/ETL_pipelines",
     author = "Matthew Teelucksingh",
-    packages = setuptools.find_packages()
+    packages = setuptools.find_packages(),
     license = 'MIT',
     long_description=open('README.md').read()   
     )
